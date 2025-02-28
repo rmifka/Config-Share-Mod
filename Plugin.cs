aae4aa421087d62ab18ae718669779598c6fd432
Mifka Rene <rene.mifka@gmail.com>
2025-02-28 20:19:25 +0100

    fix pc too fast :sob:

--- Plugin.cs
+++ Plugin.cs
@@ -42,12 +42,20 @@ namespace Config_Share
         // Resharper disable once UnusedMember.Global
         public async Task OnApplicationStart()
         {
-            Logger.Debug("OnApplicationStart");
-            new GameObject("Config_ShareController").AddComponent<Config_ShareController>();
-            new GameObject("ConfigShareMainBehaviour").AddComponent<ColorFetcher>();
-            await MainMenuAwaiter.WaitForMainMenuAsync();
-            BSMLWrapper.EnableUI();
+            Logger.Info("OnApplicationStart - Loading Config Share first!");
+
             _harmony.PatchAll(Assembly.GetExecutingAssembly());
+            GameObject controllerObject = new GameObject("Config_ShareController");
+            Object.DontDestroyOnLoad(controllerObject);
+            controllerObject.AddComponent<Config_ShareController>();
+
+            GameObject colorFetcherObject = new GameObject("ConfigShareMainBehaviour");
+            Object.DontDestroyOnLoad(colorFetcherObject);
+            colorFetcherObject.AddComponent<ColorFetcher>();
+
+            await MainMenuAwaiter.WaitForMainMenuAsync();
+
+            BSMLWrapper.EnableUI();
         }
 
         [OnExit]
